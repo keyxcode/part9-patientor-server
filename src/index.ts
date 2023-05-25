d0f8ec8077aacc10539d17115c7705f6f77193a4
keyxcode <keyxcode@gmail.com>
2023-05-25 17:07:37 +0700

    Solution 9.10: create get diagnoses route

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
+import patientorRouter from "./routes/patientorRouter";
 
 const app = express();
+app.use(express.json());
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 app.use(cors());
-app.use(express.json());
 
 const PORT = 3001;
 
@@ -12,6 +13,8 @@ app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("pong");
 });
+
+app.use("/api/diagnoses", patientorRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
